ac4a656c42e700323c8db537d2d58a38caae201c
Axel Vestberg <axel.vestberg@gmail.com>
2019-04-24 12:45:53 +0200

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+        Debug.Log("Level Complete!");
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            Invoke("Restart", restartDelay);
+            Debug.Log("We hit something!");
+        }
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    public void moveLeftPC()
+    {
+        rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    public void moveRightPC()
+    {
+        rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    public void moveLeftMobile()
+    {
+        rb.AddForce((-sidewaysForce / 2) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    public void moveRightMobile()
+    {
+        rb.AddForce((sidewaysForce / 2) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+        {
+            moveLeftPC();
+        }
+        if (Input.GetKey("d"))
+        {
+            moveRightPC();
+        }
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.position.x < Screen.width / 2)
+            {
+
+                moveLeftMobile();
+            }
+            else if (touch.position.x > Screen.width / 2)
+            {
+                moveRightMobile();
+            }
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
